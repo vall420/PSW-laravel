b37eec26b5e91d140d89c24fbf1eb1b249e03b98
Vallen Angellito <vallenangelliyo131@gmail.com>
2023-03-20 20:26:35 +0700

    project initial

---
+++ resources/views/CV.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="CV.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tugas cv vallen-2231133</title>
+</head>
+<body>
+    <div>
+        <img class="foto" src="img/pasfoto.jpeg" alt="pasfoto" width="150" class="img">
+        <h1 align="center"><b>Curriculum Vitae</b></h1>
+        <br>
+        <h1>Vallen Angellito</h1>
+        <p>mahasiswa aktif UIB</p>
+        <hr>
+        <h2>Penghargaan</h2>
+        <p>Nilai tertinggi UN Bahasa Inggris di jenjang SMP</p>
+        <h2>Contact</h2>
+        <p> no hp: 0895-6155-31696 <br>
+            Email: vallenangellito131@gmail.com <br>
+            Instagram: @Va1l3n
+        </p>
+            <h2>Pengalaman kerja</h2>
+            <h3>Designer Magang</h3>
+        <p>CV artai badai permata|2022-sekarang</p>
+            <h2>Edukasi</h2>
+            <h3>Universitas International Batam(UIB)</h3>
+            <h4>Sistem Informasi</h4>
+            <h3>SMA Maitreyawira</h3>
+            <h4>Jurusan Bahasa dan Budaya</h4>
+            <p> Merupakan salah satu penulis buku berjudul "Assorted Literature" </p>
+            <h3>SMP Maitreyawira</h3>
+        <p> Nilai tertinggi UN Bahasa Inggris di jenjang SMP</p>
+    </div>
+</body>
+</html>
